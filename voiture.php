bbe020eb523648a81373f718feac0bfa9930d3ea
FahRAN11O <famenofa@gmail.com>
2023-10-23 14:13:49 +0300

    visibility and variable scope

--- voiture.php
+++ voiture.php
@@ -3,10 +3,7 @@
     public $marque;
     public $couleur;
 
-    public function demarrer(){
-        return 'La voiture '.$this ->marque
-            .' de couleur '.$this->couleur.' demarre.';
-    }
+    
  }
 
  //Création d'un objet à partir de la clase Voiture
@@ -16,11 +13,4 @@
  $uneToyota->marque = 'Toyota';
  $uneToyota->couleur = 'Rouge';
 
- //Appel de la méthode de l'objet
- echo $uneToyota->demarrer();
  
- $uneRenault = new Voiture();
- $uneRenault->marque = 'Renault';
- $uneRenault->couleur = 'blanche';
-
- echo $uneRenault ->demarrer();
