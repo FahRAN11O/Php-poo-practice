b6a53fc2f8bf63662d6fe4ba85ce00e2ac00b41d
FahRAN11O <famenofa@gmail.com>
2023-10-25 14:51:48 +0300

    scope de variable ou visibilité , protected pour l'heritage ...

--- voiture.php
+++ voiture.php
@@ -1,16 +1,40 @@
 <?php
  class Voiture {
-    public $marque;
-    public $couleur;
+    protected $marque;
+    protected $couleur;
 
+    public function setMarque($nom){
+        $this->marque = strtoupper($nom);
+    }
+
+    public function setCouleur($nom){
+        $this->couleur = strtolower($nom);
+    }
+
+    public function getMarque(){
+        return $this->marque ;
     
+    }
+
+    public function getCouleur(){
+        return $this ->couleur;
+    }
  }
+    class Toyota extends Voiture{
+        public function setMarque($nom){
+            $this->marque='TOYOTA';
+        }
+     }
+
+
 
  //Création d'un objet à partir de la clase Voiture
- $uneToyota = new Voiture();
+ $uneToyota = new Toyota();
 
  //Définition des propriétés de l'objet
- $uneToyota->marque = 'Toyota';
- $uneToyota->couleur = 'Rouge';
+ $uneToyota->setMarque ('');
+ $uneToyota -> setCouleur('Rouge');
+ var_dump($uneToyota);
 
+ 
  
