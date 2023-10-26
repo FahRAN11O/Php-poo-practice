74f655a4db8b4d392a5e02ee77cda3d7498a343f
FahRAN11O <famenofa@gmail.com>
2023-10-26 15:30:48 +0300

    on fait ralentir dans une constante, et ajout d'interface

--- voiture.php
+++ voiture.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * classe ferrari fille de la voiture
- */
-class Ferrari extends Voiture
-{
-    
-    public function __construct()
-    {
-        parent::__construct();
-        $this -> acceleration = 10;
-    }
+
+
+
+interface Vehicule{
+    public function demarrer();
+    public function accelerer();
+    public function getVitesse();
+    public function ralentir();
 }
 
 
@@ -17,7 +14,12 @@ class Ferrari extends Voiture
  * Classe représentant une Voiture
  * Geres la marque et la couleur d'une voiture.
  */
- class Voiture {
+//Si on met une classe comme final class, elle ne peut pas être instanciée
+ class Voiture implements Vehicule {
+
+    const RALENTIR = 1;
+
+    //protected c'est pour les classes enfants
     protected float $vitesse;
 
     protected float $acceleration;
@@ -50,7 +52,38 @@ class Ferrari extends Voiture
         public function getAcceleration(){
             return $this->acceleration;
         }
+
+        public function ralentir(){
+            $this -> vitesse-=self::RALENTIR;
+        }
  }
+
+ /**
+ * classe ferrari fille de la voiture
+ */
+class Ferrari extends Voiture
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this -> acceleration = 10;
+    }
+
+    public function getVitesse(){
+        return $this->vitesse.' km/h';
+    }
+}
+
+class Toyota extends Voiture
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this -> acceleration = 5;
+    }
+}
 
  $maFerrari = new Ferrari();
  $maFerrari -> demarrer();
@@ -59,6 +92,14 @@ class Ferrari extends Voiture
  echo $maFerrari -> getVitesse(), '<br>';
  $maFerrari -> accelerer();
  echo $maFerrari -> getVitesse(), '<br>';
+
+ $maToyota = new Toyota();
+ $maToyota -> demarrer();
+ echo $maToyota -> getVitesse(), '<br>';
+ $maToyota -> accelerer();
+ echo $maToyota -> getVitesse(), '<br>';
+ $maToyota -> accelerer();
+ echo $maToyota -> getVitesse(), '<br>';
 
 
     
